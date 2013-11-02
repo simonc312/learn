ed22754f263beda319808a8081d2b661946de83c
zhirongg <zhirongg@gmail.com>
2013-11-02 16:22:25 -0700

    event calendar first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LearningShelter::Application.routes.draw do
+  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
+
   devise_for :users
 	resources :dashboard
 
