1f4bb696c3e637dd26863b44a050b6a86c4b7404
jaydeemin@gmail.com <jdmin@berkeley.edu>
2013-11-08 20:01:07 -0800

    Warning when attempting to submit additional application

--- app/controllers/dashboard/application/clients_controller.rb
+++ app/controllers/dashboard/application/clients_controller.rb
@@ -3,9 +3,13 @@ class Dashboard::Application::ClientsController < ApplicationController
     client_app = Client.create!(params[:client])
     if current_user.apps.none?
     	current_user.apps << client_app
+      flash[:notice] = "A new Client Application has been submitted!"
+      redirect_to '/dashboard'
+    else
+      flash[:notice] = "You have already submitted a Client Application."
+      redirect_to '/dashboard'
     end
-    flash[:notice] = "A new Client Application has been submitted!"
-    redirect_to '/dashboard'
+    
   end
   def index
   	@clients = Client.all
