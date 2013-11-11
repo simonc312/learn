35ca395f51b9de5e73ccbf0b72c518dc4728fabd
zhirongg <zhirongg@gmail.com>
2013-11-11 04:09:57 -0800

    rspec coverage 59%

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -4,36 +4,54 @@ describe Dashboard::Calendar::EventsController do
   before(:each) do 
     @user = FactoryGirl.create(:user)
     sign_in @user
+    @event = mock('Event', :name => 'eventname', :description => 'mydes', :id => '11')
+  end
+  describe 'get events_controller' do 
+    it 'should render new page' do
+      get :index
+      response.should be_success
+    end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
-  describe 'get dashboard/calendar/events#new' do 
+  describe 'get create path' do 
     it 'should render the new page for calendar event' do 
-      get :new
-      response.should == '/dashboard/calendar/events/new'
+      post :new
+      #puts response
+      response.should be_success
     end
-  end
-  
+    it 'calls create' do
+      new_event = FactoryGirl.build(:event)
+      Event.should_receive(:new).and_return(new_event)
+      post :create
+      assigns[:event].should eq(new_event)
+    end
+
+  describe "#create" do 
+     it 'should add event to database and redirect to home page' do
+      e = mock('Event')
+      e.stub!(:name)
+      Event.should_receive(:create!).and_return(e)
+      post :create, {:event => e}
+    end
+  end 
+
   describe 'get dashboard/calendar/events#edit' do 
-    @event = FactoryGirl.create(:event)
     it 'should render the edit page for calendar event' do 
-      get :edit, {:id => @event.id}
-      #response.should redirect_to '/dashboard/calendar/events/#{:id}'
-      page.should have_content "Edit"
+      Event.should_receive(:find).with('11').and_return(@event)
+      get :edit, {:id => '11'}
+      response.should be_success
     end
   end
-  describe 'get dashboard/calendar/events#show' do 
-    FactoryGirl.create(:event)
-    it 'should render the show page for calendar event' do 
-      get :show
-      response.should redirect_to '/dashboard/calendar/events/:id'
+  describe 'get dashboard/calendar/events#destroy' do 
+    it 'should render the edit page for calendar event' do 
+      Event.should_receive(:destroy).with('11').and_return(@event)
+      get :destroy, {:id => '11'}
+      response.should be_success
     end
   end
-  describe 'get dashboard/calendar/events#delete' do 
-    FactoryGirl.create(:event)
-    it 'should render the redirect to calendar page' do 
-      get :edit
-      response.should redirect_to '/dashboard/calendar'
-    end
-  end
-  
+
 end
