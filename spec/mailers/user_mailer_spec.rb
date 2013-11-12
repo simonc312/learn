ab0c6ec74ae8d4a133a2a3e9348533f35ba1888f
saasbook <saasbook@saasbook.(none)>
2013-11-11 19:07:09 -0800

    usermailer specs

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,11 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before {ActionMailer::Base.deliveries = []}
+  it "should send confirmation emails" do
+    user = FactoryGirl.create(:user)
+    UserMailer.welcome_email(user).deliver
+    ActionMailer::Base.deliveries.should_not be_empty
+
+  end
 end
