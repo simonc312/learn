c35a85af570d1c8dd547392a6c6b8bb0d4cd92fe
zhirongg <zhirongg@gmail.com>
2013-11-23 01:30:30 -0800

    added slideshow, fixed some css

---
+++ app/assets/javascripts/slideshow.js
@@ -0,0 +1,9 @@
+<!--
+//preload images
+var image1=new Image()
+image1.src="http://thelastmile.org/wp-content/uploads/Banner/00c-Group-Large3-1567338_960x332.png"
+var image2=new Image()
+image2.src="http://thelastmile.org/wp-content/uploads/Banner/San_Quentin_Prison-1566958_960x332.jpg"
+var image3=new Image()
+image3.src="http://thelastmile.org/wp-content/uploads/Banner/San_Quentin-263896_960x332.jpg"
+//-->
