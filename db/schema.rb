0ced6595b44f035bc5ad89f1ff63f72f3e60fdfc
zhirongg <zhirongg@gmail.com>
2013-12-02 15:31:41 -0800

    changed css for admin and calendar

--- db/schema.rb
+++ db/schema.rb
@@ -60,13 +60,9 @@ ActiveRecord::Schema.define(:version => 20131128234633) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.string   "status"
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
