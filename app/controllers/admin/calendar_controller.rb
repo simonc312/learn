9eff345a4960be7edf6da312934dbab8acb2a3e8
saasbook <saasbook@saasbook.(none)>
2013-12-09 18:57:40 -0800

    DRYed out calendar controllers for admin and dashboard to use GetEventsHelper

--- app/controllers/admin/calendar_controller.rb
+++ app/controllers/admin/calendar_controller.rb
@@ -2,12 +2,7 @@ class Admin::CalendarController < ApplicationController
   before_filter :authenticate_admin!
   include GetEventsHelper
   def index
-    @month = (params[:month] || (Time.zone || Time).now.month).to_i
-    @year = (params[:year] || (Time.zone || Time).now.year).to_i
-    @shown_month = Date.civil(@year, @month)
-    @start_d, @end_d = Event.get_start_and_end_dates(@shown_month) 
-    @events = Event.events_for_date_range(@start_d, @end_d)
-    @event_strips = Event.create_event_strips(@start_d, @end_d, @events)
+    self.getEvents
   end
   
 end
