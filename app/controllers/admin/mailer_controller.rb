148b4488b27590a221d927cd675623e7ebaaf39b
zhirongg <zhirongg@gmail.com>
2013-12-09 20:44:46 -0800

    added partial files

--- app/controllers/admin/mailer_controller.rb
+++ app/controllers/admin/mailer_controller.rb
@@ -3,13 +3,13 @@ class Admin::MailerController < ApplicationController
     @subject = params[:mail][:subject]
     @body = params[:mail][:body]
     @statuses = []
-    User.all_status.each {|status|
+    User.all_status.each do |status|
       if params[:statuses]
         if params[:statuses][status]
           @statuses << status
         end
       end
-    }
+    end
     @users = User.where("status = ?", @statuses)
     if @users.empty?
       flash[:notice] = "No users selected"
